ea166de69812478a976eb222138885cc54026fc1
narayani2327 <harikalanarayani@gmail.com>
2022-12-24 16:20:39 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './css/index.css';
 import App from './App';
-  
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+import {createRoot} from 'react-dom/client'; 
+import App from './App';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(<App />);
