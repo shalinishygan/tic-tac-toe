52669d603562119af6a452fa92938b0759866409
narayani2327 <harikalanarayani@gmail.com>
2022-12-24 16:21:07 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './css/index.css';
 import App from './App';
 import {createRoot} from 'react-dom/client'; 
-import App from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
